d90828cf57e332bcd1b3e55a2fa62f296c2ee227
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-14 06:09:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const nestedMuppet = {
 const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ")
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const [bessie, , dolly, babe, little] = armAnimals.split(" ")
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
 
